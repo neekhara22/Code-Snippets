0081226c5a612c2827727225bf4b9ed36fedb749
Akash@22 <100702414+neekhara22@users.noreply.github.com>
2022-08-20 14:44:59 +0530

    Add files via upload

---
+++ prepcoding/DataStructure/SelectionSort.java
@@ -0,0 +1,40 @@
+package DataStructure;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+
+	public static void main(String[] args) {
+		int[] arr = { 61, 2, -1, 1, -32, -67 };
+		selection(arr);
+		System.out.println(Arrays.toString(arr));
+
+	}
+
+	static void selection(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			// find the max item and swap with correct index
+			int last = arr.length - i - 1;
+			int maxIndex = getMaxIndex(arr, 0, last);
+			swap(arr, maxIndex, last);
+		}
+
+	}
+
+	private static int getMaxIndex(int[] arr, int start, int end) {
+		int max = start;
+		for (int i = start; i <= end; i++) {
+			if (arr[max] < arr[i]) {
+				max = i;
+			}
+		}
+		return max;
+	}
+
+	static void swap(int[] arr, int first, int second) {
+		int temp = arr[first];
+		arr[first] = arr[second];
+		arr[second] = temp;
+	}
+
+}
